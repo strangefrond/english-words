12c32ec55418dbb065f58fadcd0b102d3f1c0d8c
Andrew Hookom <hookom@gmail.com>
2017-04-11 14:05:23 -0400

    'word parsing implemented'

---
+++ wordparser.js
@@ -0,0 +1,33 @@
+let fs = require('fs');
+
+let { Dictionary } = require('./Dictionary.js');
+
+let dictionary = new Dictionary();
+
+let words = fs.readFileSync('./words3.txt',{encoding:'utf8'});
+
+let wordArr = words.split('\n');
+
+let caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let lower = caps.toLowerCase();
+
+let fourLetterWords = wordArr.filter(word=>word.length===4);
+
+let validFourLetterWords = fourLetterWords.filter(word=>{
+  return word.split('').filter(letter=>lower.includes(letter)).length===4});
+
+validFourLetterWords = validFourLetterWords.map(word=>word.toUpperCase());
+
+//run some tests on the dictionary
+// let snippet = validFourLetterWords.slice(0,10);
+// dictionary.bulkAdd(snippet);
+
+// let firstResult = dictionary.search('AAHS');
+// let secondResult = dictionary.search('ABBE');
+
+// console.log(snippet,firstResult,secondResult);
+
+dictionary.bulkAdd(validFourLetterWords);
+
+fs.writeFileSync('../../../Slam/Dictionary.txt',`export default JSON.parse(${JSON.stringify(dictionary)}`);
